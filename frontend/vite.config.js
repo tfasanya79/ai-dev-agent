6e7cafc96751f1492c425d52c2b31dd01347e349
Tim Fasanya <timothy.fasanya79@gmail.com>
2025-05-30 19:07:41 +0200

    Error resolved: vite.config.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        rollupNodePolyFill(),
-      ],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Specific polyfills you need
+      include: ['fs', 'path', 'process', 'stream', 'crypto'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      crypto: 'crypto-browserify',
     },
   },
   define: {
@@ -20,6 +33,13 @@ export default defineConfig({
       define: {
         global: 'globalThis',
       },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
 });
